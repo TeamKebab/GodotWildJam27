5bd247238487044ae5dbbdf4f1680962ec56855b
Ángela López <angela.lbsr@gmail.com>
2020-11-19 19:50:14 +0100

    refactor spawner

--- save-the-hangman/bichitos/bichito_spawner.gd
+++ save-the-hangman/bichitos/bichito_spawner.gd
@@ -1,40 +1,24 @@
 extends Node2D
 
 
-const Bichito = preload("res://bichitos/bichito_primero.tscn")
-const BichitoRotador = preload("res://bichitos/bichito_cabron_rotador.tscn")
-
-export var max_bichito_primero:int = 3
-export var max_bichito_rotador:int = 2
+export(PackedScene) var bichito_scene
+export var max_bichitos:int = 3
 
 onready var _screen_size = get_viewport().size
 
-onready var _primeros_container = $BichitosPrimeros
-onready var _rotador_container = $BichitosRotadores
+onready var _container = $Bichitos
 onready var _timer = $Timer
 
-onready var _config = {
-	"primero": {
-		"max": max_bichito_primero,
-		"container": _primeros_container,
-		"scene": Bichito
-	},
-	"rotador": {
-		"max": max_bichito_rotador,
-		"container": _rotador_container,
-		"scene": BichitoRotador
-	}
-}
 
 func _ready():
 	_timer.connect("timeout", self, "_on_timer_timeout")
 	
 	
-func _spawn(config):
-	var bichito = config.scene.instance()
+func _spawn():
+	var bichito = bichito_scene.instance()
 	bichito.global_position = _screen_size.abs() * Random.direction()
 	
-	config.container.add_child(bichito)
+	_container.add_child(bichito)
 	bichito.connect("died", self, "_on_bichito_died")
 	
 
@@ -44,8 +28,6 @@ func _on_bichito_died(_bichito):
 	
 	
 func _on_timer_timeout():
-	for key in _config:
-		var config = _config[key]
-		if config.container.get_child_count() < config.max:
-			_spawn(config)
-			_timer.start()
+	if _container.get_child_count() < max_bichitos:
+		_spawn()
+		_timer.start()
