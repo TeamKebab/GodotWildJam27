78b87a12bad43d2e6158362dead8924d922f4383
Ángela López <angela.lbsr@gmail.com>
2020-11-21 16:17:44 +0100

    booky

---
+++ save-the-hangman/booky/booky.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+const LOOK_ANIMATIONS = [
+	"LookUp",
+	"LookDown",
+	"LookLeft",
+	"LookRight",
+	"LookFront",
+	"LookFront",
+	"LookFront",
+	"LookFront",	
+]
+
+onready var look_around_timer = $LookAroundTimer
+onready var look_around_animation = $LookAroundAnimation
+
+onready var blink_timer = $BlinkTimer
+onready var blink_animation = $BlinkAnimation
+
+onready var talk_timer = $TalkTimer
+onready var mouth = $Mouth
+
+func _ready():
+	look_around_timer.connect("timeout", self, "look_somewhere")
+	blink_timer.connect("timeout", self, "blink")
+	talk_timer.connect("timeout", self, "talk")
+		
+	look_somewhere()
+	blink()
+	start_talking()
+
+
+func look_somewhere():
+	var direction = Random.choose(LOOK_ANIMATIONS)	
+	look_around_animation.play(direction)
+	
+	var time = Random.randf_range(1,3)
+	
+	look_around_timer.start(time)
+
+
+func blink():
+	blink_animation.play("Blink")
+	
+	yield(get_tree().create_timer(0.4, 3), "timeout")
+	
+	blink_animation.play_backwards("Blink")
+	
+	var time = Random.randf_range(6,15)
+	
+	blink_timer.start(time)
+
+
+func start_talking():
+	talk_timer.start()
+	
+	
+func stop_talking():
+	talk_timer.stop()
+	mouth.frame = 3
+	
+	
+func talk():
+	mouth.frame = Random.randi(3)
+	
+	
--- save-the-hangman/ui/narration_box.gd
+++ save-the-hangman/ui/narration_box.gd
@@ -1,43 +1,64 @@
 extends MarginContainer
 
-
+export var talk_speed = 20
 export(Array, String) var lines: Array
 
+var time = 0
 var can_advance = false
 var current_line = 0
 
 onready var text_label = find_node("TextLabel")
 onready var next_line = find_node("NextLine")
+onready var booky = find_node("Booky")
 
 func _ready():
-	text_label.text = lines[0]	
-	
-	yield(get_tree().create_timer(1), "timeout")
-	can_advance = true
-	next_line.show()
+	start_line()
 
 
 func _input(event):
 	if can_advance:
 		if event is InputEventKey or event is InputEventMouseButton:
 			next_line()
-			
+
+
+func _process(delta):
+	if current_line >= lines.size():
+		return	
+		
+	var total_letters = lines[current_line].length()
+	
+	time += delta
+	
+	text_label.visible_characters = floor(time * talk_speed)
+	
+	if text_label.visible_characters >= total_letters:
+		booky.stop_talking()
+		
+		if current_line < lines.size() - 1:
+			next_line.show()
+			can_advance = true
+		
+
+func start_line():
+	text_label.text = lines[current_line]	
+	text_label.visible_characters = 0
+	can_advance = false
+	time = 0
+	next_line.hide()
+	booky.start_talking()			
+
 
 func next_line():
+		
 	current_line += 1
 	
 	if current_line >= lines.size():
 		return	
 	
-	text_label.text = lines[current_line]
-	can_advance = false
-	next_line.hide()
-		
+	start_line()
+	
 	if current_line == lines.size() - 1:
 		find_parent("Level").start()
-	else:	
-		yield(get_tree().create_timer(1), "timeout")
-		can_advance = true
-		next_line.show()
+
 					
-	
+		
